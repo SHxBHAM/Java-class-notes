afefeae47396eb4bb48c7366e2d2d7f3d5436f66
SHxBHAM <shubhammishra41880@gmail.com>
2024-01-18 12:13:03 +0530

    class notes

---
+++ Arithmetics.java
@@ -0,0 +1,8 @@
+public class Arithmetics {
+    public static void main(String[] args){
+        System.out.println(7+7);
+        System.out.println(7-7);
+        System.out.println(7*7);
+        System.out.println(7/7);
+    }
+}
